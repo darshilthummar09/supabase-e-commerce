9e16b33ed37f3cb525046c6c46dc4062685087d4
darshil thummar <darshilthummar952002@gmail.com>
2026-08-23 11:43:59 +0530

    chore: configure root base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/supabase-e-commerce/' : '/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
